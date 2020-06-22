e2e25aa0bb54961c6de8582d73db862ac9b068ab
ksar <35605507+ksar-ksar@users.noreply.github.com>
2020-06-22 15:31:05 +0200

    Add comments

--- doli_install.php
+++ doli_install.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2020       ksar    <ksar.ksar@gmail.com>
+ * From an original idea of elarifr accedinfo.com
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
